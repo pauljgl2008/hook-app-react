996248b923f7b8cbc79fe1b1e85eef13f506bc51
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-10 17:35:52 -0500

    feat: multi state of counters 1,2,3

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './Hooks'
+import { CounterApp } from './01-useState/CounterApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp />
+    <CounterApp />
   </StrictMode>,
 )
