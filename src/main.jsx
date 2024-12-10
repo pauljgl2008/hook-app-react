06c3a39bee792752b489fba8bb2a2050269b83f6
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-10 17:47:53 -0500

    feat: useCounter - customHook

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './Hooks'
-import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './Hooks'
+// import { CounterApp } from './01-useState/CounterApp'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </StrictMode>,
 )
