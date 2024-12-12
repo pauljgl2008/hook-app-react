ad023594af0124120c6b494f61dd3b1ee6664d6e
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-11 22:15:56 -0500

    feat: useFetch + useCounter, sprites, pokemons

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,14 +1,35 @@
-import { useFetch } from '../hooks'
+import { useCounter, useFetch } from '../hooks'
+import { LoadingMessage } from './LoadingMessage';
+import { PokemonCard } from './PokemonCard';
 
 export const MultipleCustomHooks = () => {
 
-  const { data, hasError, isLoading } = useFetch('https://api.frankfurter.dev/v1/latest');
+  const { counter, decrement, increment } = useCounter(1)
+  const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
   return (
     <>
       <div>MultipleCustomHooks</div>
       <hr />
-      {isLoading && <p>Cargando...</p>}
-      <h2>{data?.date}</h2>
+      {isLoading ? <LoadingMessage /> : <PokemonCard
+        id={data.id}
+        name={data.name}
+        sprites={[
+          data.sprites.front_default,
+          data.sprites.front_shiny,
+          data.sprites.back_default,
+          data.sprites.back_shiny
+        ]} />
+      }
+      <button
+        onClick={() => counter > 1 ? decrement() : null}
+        className='btn btn-primary mt-2'>
+        Anterior
+      </button>
+      <button
+        onClick={() => increment()}
+        className='btn btn-primary mt-2'>
+        Siguiente
+      </button>
     </>
   )
 }
