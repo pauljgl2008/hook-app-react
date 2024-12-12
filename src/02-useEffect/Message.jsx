7828e98bbceefcd1b11e4cc9f57f16b4ebbc1009
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-11 19:12:58 -0500

    feat: useEffect precautions

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,17 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
-      console.log('Message Mounted');
-      return () => {
-        console.log('Message Unmounted');
-      }
+        const onMouseMove = ({ x, y }) => {
+            // const coords = { x, y };
+            setCoords({ x, y })
+        }
+        window.addEventListener('mousemove', onMouseMove)
+        return () => {
+            window.removeEventListener('mousemove', onMouseMove)
+        }
     }, [])
-    
-  return (
-    <>
-    <h3>Usuario ya existe</h3>
-    </>
-  )
+
+    return (
+        <>
+            <h3>Usuario ya existe</h3>
+            {JSON.stringify(coords)}
+        </>
+    )
 }
