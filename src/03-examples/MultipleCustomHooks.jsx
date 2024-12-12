2d61fd496bf33c00e210cb4949221cae64b1773a
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-11 21:13:13 -0500

    feat: Parameterize and consume our custom Hook

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,14 @@
-import {useFetch} from '../hooks'
+import { useFetch } from '../hooks'
 
 export const MultipleCustomHooks = () => {
 
-  useFetch();
+  const { data, hasError, isLoading } = useFetch('https://api.frankfurter.dev/v1/latest');
   return (
-    <div>MultipleCustomHooks</div>
+    <>
+      <div>MultipleCustomHooks</div>
+      <hr />
+      {isLoading && <p>Cargando...</p>}
+      <h2>{data?.date}</h2>
+    </>
   )
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const useFetch = () => {
+export const useFetch = (url) => {
     const [state, setState] = useState({
         data: null,
         isLoading: true,
@@ -10,13 +10,42 @@ export const useFetch = () => {
 
     useEffect(() => {
         getFetch();
-    }, []);
+    }, [url]);
 
-    const getFetch = async()=>{
-       const resp = await fetch('https://api.frankfurter.dev/v1/latest')
-       const data = await resp.json();
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null
+        })
+    }
 
-       console.log(data);
+    const getFetch = async () => {
+        setLoadingState();
+        const resp = await fetch(url);
+        //sleep
+        await new Promise(resolve => setTimeout(resolve, 1500));
+        if (!resp.ok) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: resp.status,
+                    message: resp.statusText
+                }
+            });
+            return;
+        }
+
+        const data = await resp.json();
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null
+        })
     }
 
     return {
