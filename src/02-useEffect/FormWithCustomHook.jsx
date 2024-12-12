802f7ca6406af8b1992a6b5bdd88962c389c5e8f
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-11 19:26:33 -0500

    feat: useForm with custom hook, username, email, password

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHook = () => {
+
+    const { formState, onInputChange, username, email, password } = useForm({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    return (
+        <>
+            <h1>Formulario con custom hook</h1>
+            <hr />
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="paulguevara@gmail.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+            <input
+                type="password"
+                className="form-control mt-2"
+                placeholder="Contraseña"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+            />
+            {(username === 'test2') && <Message />}
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { HooksApp } from './Hooks'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   </StrictMode>,
 )
