33a9764e19309b211976dc56de81f50921bd8903
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-14 17:32:06 -0500

    feat: browserRouter v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { MainApp } from './09-useContext/MainApp'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
 // import { MemoHook } from './06-memos/MemoHook'
@@ -14,10 +15,15 @@ import './index.css'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
+// import { TodoApp } from './08-useReducer/TodoApp'
+import {
+  BrowserRouter
+} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <TodoApp />
-  // </StrictMode>,
+  <BrowserRouter>
+    {/* <StrictMode> */}
+      <MainApp />
+    {/* </StrictMode> */}
+  </BrowserRouter>
 )
