4f8a023263face8ff30a030bdc53375cb7c59903
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-15 17:08:15 -0500

    feat: Navbar component

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,14 +1,13 @@
-import { Navigate, Route, Routes, Link } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AboutPage, HomePage, LoginPage } from "./index"
+import { Navbar } from "./Navbar"
 
 export const MainApp = () => {
     return (
         <>
             <h1>MainApp</h1>
             <hr />
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/login">Login</Link>
+            <Navbar />
             <hr />
             <Routes>
                 <Route path="/" element={<HomePage />} />
@@ -16,7 +15,7 @@ export const MainApp = () => {
                 <Route path="about" element={<AboutPage />} />
                 {/* <Route path="/*" element={<LoginPage />} /> */}
 
-                <Route path="/*" element={<Navigate to="/about"/>}/>
+                <Route path="/*" element={<Navigate to="/about" />} />
             </Routes>
         </>
     )
