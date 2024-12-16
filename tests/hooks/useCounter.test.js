8fd37b6c9a84297f0edf5f3d84fcf8860f1ef99d
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-15 21:44:16 -0500

    feat: test useCounter, increment, decrement, reset

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,5 +1,5 @@
-import { renderHook } from '@testing-library/react'
-import { useCounter} from '../../src/hooks/index'
+import { act, renderHook } from '@testing-library/react'
+import { useCounter } from '../../src/hooks/index'
 
 describe('Pruebas en el useCounter', () => {
     test('debe de retornar los valores por defecto', () => {
@@ -10,9 +10,36 @@ describe('Pruebas en el useCounter', () => {
         expect(increment).toEqual(expect.any(Function));
         expect(reset).toEqual(expect.any(Function));
     })
-    test('debe de generar el counter con el valor de 100', () => { 
+    test('debe de generar el counter con el valor de 100', () => {
         const { result } = renderHook(() => useCounter(100));
         const { counter } = result.current;
         expect(counter).toBe(100);
-     })
+    })
+    test('debe de incrementar el contador', () => {
+        const { result } = renderHook(() => useCounter());
+        const { counter, increment } = result.current;
+        act(() => {
+            increment()
+            increment(2)
+        });
+        expect(result.current.counter).toBe(13);
+    })
+    test('debe de decrementar el contador', () => {
+        const { result } = renderHook(() => useCounter());
+        const { counter, decrement } = result.current;
+        act(() => {
+            decrement()
+            decrement(2)
+        });
+        expect(result.current.counter).toBe(7);
+    })
+    test('debe de resetear el contador', () => {
+        const { result } = renderHook(() => useCounter());
+        const { counter, decrement, reset } = result.current;
+        act(() => {
+            decrement(1)
+            reset()
+        });
+        expect(result.current.counter).toBe(10);
+    })
 })
